2515381828ab7669d56335de837cd9ce874da9cc
dcwhy <1150215282@qq.com>
2020-10-28 15:02:14 +0800

    why

--- components/Home/functionbox.vue
+++ components/Home/functionbox.vue
@@ -2,11 +2,11 @@
 	<view class="funbox">
 		<view class="right_box">
 			<view class="headbox">
-				123456
+				<image class="headimg" src="https://profile.csdnimg.cn/6/2/8/3_leizi929" mode=""></image>
 			</view>
 			<image @click="changexihuan" v-if="show===0" class="usicon" src="../../static/homeimg/xihuan@x1.png" mode=""></image>
 			<image @click="changexihuan" v-if="show===1" class="usicon" src="../../static/homeimg/xihuan@x2.png" mode=""></image>
-			<image class="usicon" src="../../static/homeimg/pinglun@x1.png" mode=""></image>
+			<image @click="getpinlun" class="usicon" src="../../static/homeimg/pinglun@x1.png" mode=""></image>
 			<image class="usicon" src="../../static/homeimg/zhuanfa@x1.png" mode=""></image>
 		</view>
 	</view>
@@ -26,6 +26,9 @@
 				}else{
 					this.show=0
 				}
+			},
+			getpinlun(){
+				
 			}
 		}
 	}
@@ -36,8 +39,16 @@
 		width: 70rpx;
 		height: 70rpx;
 		margin-bottom: 30rpx;
-		border: 3px solid #FFFFFF;
+		background-color: white;
 		z-index: 10000;
+		border-radius: 50%;
+		padding: 4rpx;
+		overflow: hidden;
+	}
+	.headimg{
+		width: 66rpx;
+		height: 66rpx;
+		border-radius: 50%;
 	}
 	.funbox{
 		position: relative;
